4f2cfc918a9b1b4f18ae12d525b525c2fcd94cd2
valrey <vikei.ar03@gmail.com>
2026-08-11 12:23:47 -0600

    desafio 1

---
+++ desafio_1.py
@@ -0,0 +1,9 @@
+# Array con elementos duplicados
+numeros = [1, 2, 3, 2, 4, 1, 5, 3, 6]
+
+# Eliminar elementos duplicados
+numeros_unicos = list(set(numeros))
+
+# Mostrar resultado
+print("Array original:", numeros)
+print("Array sin duplicados:", numeros_unicos)
